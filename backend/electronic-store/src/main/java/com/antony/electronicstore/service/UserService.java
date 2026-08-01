82591776d0ddff7ec8e17be1b352dfd5c1d1a107
josephantony <josephantony0763@gmail.com>
2026-08-01 19:03:43 +0530

    feat: implement user registration service

---
+++ backend/electronic-store/src/main/java/com/antony/electronicstore/service/UserService.java
@@ -0,0 +1,28 @@
+package com.antony.electronicstore.service;
+
+import com.antony.electronicstore.entity.User;
+import com.antony.electronicstore.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public  UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+    public User registerUser(User user) {
+
+        Optional<User> existingUser =
+                userRepository.findByEmail(user.getEmail());
+
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Account already exist");
+        }
+        return userRepository.save(user);
+    }
+
+}
